04d5bc19a41311b42fc90236cee5605f9fa2a7f7
Dhans <sgdhans@gmail.com>
2022-01-24 12:42:49 +0530

    hashing and signup

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -1,3 +1,23 @@
 import { Request, Response } from "express";
+import User from "../models/user";
+import bcrypt from "bcrypt";
 
-export const signup = (req: Request, res: Response) => {};
+export const signup = (req: Request, res: Response) => {
+  User.findOne({ email: req.body.username }).exec(async (error, user) => {
+    //if already registered
+    if (user) {
+      return res.status(400).json({
+        message: "username already registered",
+      });
+    }
+    //create one if not
+    const { username, password } = req.body;
+
+    const hash_password = await bcrypt.hash(password, 10);
+
+    const _user = new User({
+      username,
+      hash_password,
+    });
+  });
+};
---
+++ src/models/user.ts
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+import { UserModel } from "../interfaces/models/user";
+
+const userSchema = new mongoose.Schema(
+  //does mongoose even understands interfaces? lol
+  {
+    username: {
+      type: String,
+      required: true,
+      trim: true,
+      min: 3,
+      max: 15,
+    },
+    hash_password: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+userSchema.methods = {
+  authenticate: async function (password: string) {
+    return await bcrypt.compare(password, this.hash_password);
+  },
+};
+export default mongoose.model<UserModel>("User", userSchema);
