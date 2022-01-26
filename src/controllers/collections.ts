451c5a5e04f9a7f0c6e3c63f682b0bd40cfec57d
Dhans <sgdhans@gmail.com>
2022-01-26 14:06:53 +0530

    updation of details array

--- src/controllers/collections.ts
+++ src/controllers/collections.ts
@@ -6,9 +6,28 @@ export const addCollection = (req: Request, res: Response) => {
     (error, collection) => {
       if (error) if (error) return res.status(400).json({ error });
       if (collection) {
-        console.log("ill be updating your collection");
+        // this user has a collection
+        let showNameInRequest = req.body.collectionDetails.name;
+        /**find if we have a show that matches with this. if no push the req show in this arr */
+
+        for (let i = 0; i < collection.collectionDetails.length; i++) {
+          if (collection.collectionDetails[i].name == showNameInRequest) {
+            return res.status(400).json({
+              message: "this show already exists in your collection buddy",
+            });
+          }
+        }
+
+        collection.collectionDetails.push(req.body.collectionDetails);
+        return res.status(200).json({
+          message: "added your new show!",
+          show: req.body.collectionDetails,
+        });
+
+        // let user = Collection.findOneAndUpdate({
+        //   username: req.body.tokenId._id,
+        // } );
       } else {
-        console.log("ill be creating collection");
         const collection = new Collection({
           username: req.body.tokenId._id,
           collectionDetails: req.body.collectionDetails,
