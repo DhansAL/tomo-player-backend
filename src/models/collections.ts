6f41328897ebad7edc61b7e7745d408749cb34a3
Dhans <sgdhans@gmail.com>
2022-01-26 09:36:05 +0530

    collection schema

---
+++ src/models/collections.ts
@@ -0,0 +1,36 @@
+import mongoose from "mongoose";
+
+const collectionSchema = new mongoose.Schema(
+  {
+    //BARE BASIC COLLECTION SCHEMA
+    collection: [
+      {
+        collectionName: {
+          type: String,
+          required: true,
+        },
+        username: {
+          type: mongoose.Schema.Types.ObjectId,
+          required: true,
+          ref: "User",
+          trim: true,
+          min: 3,
+          max: 15,
+        },
+        rounds: [
+          {
+            //lets store proper dates strings
+            started: { type: String, required: true },
+            ended: { type: String, required: true },
+            round: { type: Number, required: true },
+          },
+        ],
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("Cart", collectionSchema);
